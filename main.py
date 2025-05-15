1b8e47f3962d27d818c3b741c7732263ec07befb
MinLee0210 <bastreet124@gmail.com>
2025-05-15 12:26:21 +0700

    add utils for downloading youtube video and add producer

--- main.py
+++ main.py
@@ -1,6 +1,55 @@
-def main():
-    print("Hello from kafka-producer!")
+from pathlib import Path
+import cv2
+import numpy as np
+from kafka import KafkaProducer
+
+from .utils import download_youtube_video
+
+def np_to_bytes(arr: np.ndarray):
+    return arr.tobytes()
+
+
+def filter_video_name(video_name: str): 
+    if not video_name: 
+        raise ValueError("Video name is required")
+
+    match video_name: 
+        case video_name.startswith("rtsp://"):
+            print("Running rtsp video")
+            return video_name
+        case video_name.startswith("http"): 
+            if "youtube" in video_name: 
+                print("Running youtube video")
+                download_youtube_video(video_name, "./")
+                return Path(video_name.split("/")[-1]).name
+            else: 
+                print("Running http video")
+                return video_name
+        case _:
+            print("Running local video")
+            return video_name
+
+def main(vido_name: str, 
+         topic: str = "VIDEO_STREAM",
+         bootstrap_servers: list[str] = ["localhost:9092"]):
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    video_name = filter_video_name(vido_name)
+    cap = cv2.VideoCapture(video_name)
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Encode frame as JPEG and convert to bytes
+        ret2, buffer = cv2.imencode('.jpg', frame)
+        if not ret2:
+            continue
+        frame_bytes = np_to_bytes(buffer)
+        producer.send(topic=topic, value=frame_bytes)    
+    cap.release()
+    producer.close()
 
 
 if __name__ == "__main__":
-    main()
+    video_name = ""
+    main(video_name)
